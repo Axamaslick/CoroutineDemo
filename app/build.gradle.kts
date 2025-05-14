5309c3bb48163a248517a63580d7b0786bf7a33a
Lev <kartoxauaua@gmail.com>
2025-05-14 23:30:29 +0700

    Создание интерфейса

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
 }
